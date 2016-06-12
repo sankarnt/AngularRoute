5b2b1247edce45a8886c17e1b25c702ec7752280
sankar <sankarnt@yahoo.co.in>
2016-06-13 01:05:39 +0530

    states added

--- js/app.js
+++ js/app.js
@@ -9,18 +9,28 @@ var userApp = angular.module('userApp', ['ui.router']);
 
 
 userApp.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state("userList", {
-        views: {
-            "demo1": {
-                templateUrl: "templates/home.html"
-            },
-            "demo2": {
-                template: "helooo"
-            }
-        },
+    $stateProvider.state("userlist", {
+        // views: {
+        //     "demo1": {
+        //         templateUrl: "templates/home.html"
+        //     },
+        //     "demo2": {
+        //         template: "<p>heloo</p>"
+        //     }
+        // },
+        templateUrl: "templates/home.html",
         url: "/dashboard",
         controller: "userListController"
-    });
+    }).
+    state("userlist.registration",{
+        url: "/registration",
+        templateUrl: "templates/registration.html",
+        // views: {
+        //     "demo2": {
+        //         templateUrl: "templates/registration.html"
+        //     }
+        // }
+    })
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/dashboard');
 });
